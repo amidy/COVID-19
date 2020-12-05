037e6c59cb9566ea616b9b7bf89bb04213539cca
MarlaBu <letmailmarla@gmail.com>
2020-12-05 13:25:19 -0500

    12.5

--- COVID-19/Survey Interface.Designer.cs
+++ COVID-19/Survey Interface.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.PageLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.lblQuestion4 = new System.Windows.Forms.Label();
+            this.lblQuestion3 = new System.Windows.Forms.Label();
+            this.lblQuestion2 = new System.Windows.Forms.Label();
             this.grpboxHeader = new System.Windows.Forms.GroupBox();
             this.lblHeader = new System.Windows.Forms.Label();
             this.grpbxFooter = new System.Windows.Forms.GroupBox();
-            this.btnBack = new System.Windows.Forms.Button();
             this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblQuestion1 = new System.Windows.Forms.Label();
             this.PageLayout.SuspendLayout();
             this.grpboxHeader.SuspendLayout();
             this.grpbxFooter.SuspendLayout();
@@ -45,22 +49,59 @@
             this.PageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.PageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
             this.PageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.PageLayout.Controls.Add(this.lblQuestion4, 1, 4);
+            this.PageLayout.Controls.Add(this.lblQuestion3, 1, 3);
+            this.PageLayout.Controls.Add(this.lblQuestion2, 1, 2);
             this.PageLayout.Controls.Add(this.grpboxHeader, 0, 0);
-            this.PageLayout.Controls.Add(this.grpbxFooter, 0, 6);
+            this.PageLayout.Controls.Add(this.grpbxFooter, 0, 5);
+            this.PageLayout.Controls.Add(this.lblQuestion1, 1, 1);
             this.PageLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PageLayout.Location = new System.Drawing.Point(0, 0);
             this.PageLayout.Name = "PageLayout";
-            this.PageLayout.RowCount = 7;
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.83537F));
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.647495F));
-            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.61596F));
+            this.PageLayout.RowCount = 6;
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.00609F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.06141F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.80982F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.80982F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.80982F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.503045F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.PageLayout.Size = new System.Drawing.Size(814, 539);
             this.PageLayout.TabIndex = 0;
             this.PageLayout.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // lblQuestion4
+            // 
+            this.lblQuestion4.AutoSize = true;
+            this.lblQuestion4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestion4.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion4.Location = new System.Drawing.Point(165, 395);
+            this.lblQuestion4.Name = "lblQuestion4";
+            this.lblQuestion4.Size = new System.Drawing.Size(482, 101);
+            this.lblQuestion4.TabIndex = 5;
+            this.lblQuestion4.Text = "Question 4";
+            // 
+            // lblQuestion3
+            // 
+            this.lblQuestion3.AutoSize = true;
+            this.lblQuestion3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestion3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion3.Location = new System.Drawing.Point(165, 294);
+            this.lblQuestion3.Name = "lblQuestion3";
+            this.lblQuestion3.Size = new System.Drawing.Size(482, 101);
+            this.lblQuestion3.TabIndex = 4;
+            this.lblQuestion3.Text = "Question 3";
+            // 
+            // lblQuestion2
+            // 
+            this.lblQuestion2.AutoSize = true;
+            this.lblQuestion2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestion2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion2.Location = new System.Drawing.Point(165, 193);
+            this.lblQuestion2.Name = "lblQuestion2";
+            this.lblQuestion2.Size = new System.Drawing.Size(482, 101);
+            this.lblQuestion2.TabIndex = 3;
+            this.lblQuestion2.Text = "Question 2";
             // 
             // grpboxHeader
             // 
@@ -70,7 +111,7 @@
             this.grpboxHeader.Dock = System.Windows.Forms.DockStyle.Fill;
             this.grpboxHeader.Location = new System.Drawing.Point(3, 3);
             this.grpboxHeader.Name = "grpboxHeader";
-            this.grpboxHeader.Size = new System.Drawing.Size(808, 89);
+            this.grpboxHeader.Size = new System.Drawing.Size(808, 74);
             this.grpboxHeader.TabIndex = 0;
             this.grpboxHeader.TabStop = false;
             // 
@@ -79,7 +120,7 @@
             this.lblHeader.AutoSize = true;
             this.lblHeader.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHeader.ForeColor = System.Drawing.Color.White;
-            this.lblHeader.Location = new System.Drawing.Point(309, 18);
+            this.lblHeader.Location = new System.Drawing.Point(307, 32);
             this.lblHeader.Name = "lblHeader";
             this.lblHeader.Size = new System.Drawing.Size(189, 29);
             this.lblHeader.TabIndex = 0;
@@ -93,26 +134,11 @@
             this.grpbxFooter.Controls.Add(this.btnSubmit);
             this.grpbxFooter.Controls.Add(this.btnBack);
             this.grpbxFooter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.grpbxFooter.Location = new System.Drawing.Point(3, 473);
+            this.grpbxFooter.Location = new System.Drawing.Point(3, 499);
             this.grpbxFooter.Name = "grpbxFooter";
-            this.grpbxFooter.Size = new System.Drawing.Size(808, 63);
+            this.grpbxFooter.Size = new System.Drawing.Size(808, 37);
             this.grpbxFooter.TabIndex = 1;
             this.grpbxFooter.TabStop = false;
-            // 
-            // btnBack
-            // 
-            this.btnBack.BackColor = System.Drawing.Color.Teal;
-            this.btnBack.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
-            this.btnBack.FlatAppearance.BorderSize = 3;
-            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnBack.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(50, 17);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 37);
-            this.btnBack.TabIndex = 0;
-            this.btnBack.Text = "Back";
-            this.btnBack.UseVisualStyleBackColor = false;
             // 
             // btnSubmit
             // 
@@ -122,13 +148,39 @@
             this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnSubmit.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubmit.ForeColor = System.Drawing.Color.White;
-            this.btnSubmit.Location = new System.Drawing.Point(686, 17);
+            this.btnSubmit.Location = new System.Drawing.Point(707, 6);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(75, 37);
+            this.btnSubmit.Size = new System.Drawing.Size(74, 25);
             this.btnSubmit.TabIndex = 1;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = false;
             this.btnSubmit.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Teal;
+            this.btnBack.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btnBack.FlatAppearance.BorderSize = 3;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBack.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(33, 6);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(69, 25);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // lblQuestion1
+            // 
+            this.lblQuestion1.AutoSize = true;
+            this.lblQuestion1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestion1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion1.Location = new System.Drawing.Point(165, 80);
+            this.lblQuestion1.Name = "lblQuestion1";
+            this.lblQuestion1.Size = new System.Drawing.Size(482, 113);
+            this.lblQuestion1.TabIndex = 2;
+            this.lblQuestion1.Text = "Question 1";
             // 
             // Survey_Interface
             // 
@@ -142,6 +194,7 @@
             this.Text = "Survey_Interface";
             this.Load += new System.EventHandler(this.Survey_Interface_Load);
             this.PageLayout.ResumeLayout(false);
+            this.PageLayout.PerformLayout();
             this.grpboxHeader.ResumeLayout(false);
             this.grpboxHeader.PerformLayout();
             this.grpbxFooter.ResumeLayout(false);
@@ -157,5 +210,9 @@
         private System.Windows.Forms.GroupBox grpbxFooter;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblQuestion4;
+        private System.Windows.Forms.Label lblQuestion3;
+        private System.Windows.Forms.Label lblQuestion2;
+        private System.Windows.Forms.Label lblQuestion1;
     }
 }
