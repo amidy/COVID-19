3b18200d99fc548c3fc37e0670fe21a0549ff907
MarlaBu <letmailmarla@gmail.com>
2020-12-05 12:48:11 -0500

    12.5

---
+++ COVID-19/Survey Interface.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace COVID_19
+{
+    partial class Survey_Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PageLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.grpboxHeader = new System.Windows.Forms.GroupBox();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.grpbxFooter = new System.Windows.Forms.GroupBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.PageLayout.SuspendLayout();
+            this.grpboxHeader.SuspendLayout();
+            this.grpbxFooter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PageLayout
+            // 
+            this.PageLayout.ColumnCount = 3;
+            this.PageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.PageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.PageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.PageLayout.Controls.Add(this.grpboxHeader, 0, 0);
+            this.PageLayout.Controls.Add(this.grpbxFooter, 0, 6);
+            this.PageLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PageLayout.Location = new System.Drawing.Point(0, 0);
+            this.PageLayout.Name = "PageLayout";
+            this.PageLayout.RowCount = 7;
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.83537F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.2439F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.647495F));
+            this.PageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.61596F));
+            this.PageLayout.Size = new System.Drawing.Size(814, 539);
+            this.PageLayout.TabIndex = 0;
+            this.PageLayout.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // grpboxHeader
+            // 
+            this.grpboxHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.PageLayout.SetColumnSpan(this.grpboxHeader, 3);
+            this.grpboxHeader.Controls.Add(this.lblHeader);
+            this.grpboxHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpboxHeader.Location = new System.Drawing.Point(3, 3);
+            this.grpboxHeader.Name = "grpboxHeader";
+            this.grpboxHeader.Size = new System.Drawing.Size(808, 89);
+            this.grpboxHeader.TabIndex = 0;
+            this.grpboxHeader.TabStop = false;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.White;
+            this.lblHeader.Location = new System.Drawing.Point(309, 18);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(189, 29);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Complete Form";
+            this.lblHeader.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // grpbxFooter
+            // 
+            this.grpbxFooter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.PageLayout.SetColumnSpan(this.grpbxFooter, 3);
+            this.grpbxFooter.Controls.Add(this.btnSubmit);
+            this.grpbxFooter.Controls.Add(this.btnBack);
+            this.grpbxFooter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpbxFooter.Location = new System.Drawing.Point(3, 473);
+            this.grpbxFooter.Name = "grpbxFooter";
+            this.grpbxFooter.Size = new System.Drawing.Size(808, 63);
+            this.grpbxFooter.TabIndex = 1;
+            this.grpbxFooter.TabStop = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Teal;
+            this.btnBack.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btnBack.FlatAppearance.BorderSize = 3;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBack.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(50, 17);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 37);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Teal;
+            this.btnSubmit.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btnSubmit.FlatAppearance.BorderSize = 3;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubmit.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(686, 17);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 37);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Survey_Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(814, 539);
+            this.Controls.Add(this.PageLayout);
+            this.Name = "Survey_Interface";
+            this.Text = "Survey_Interface";
+            this.Load += new System.EventHandler(this.Survey_Interface_Load);
+            this.PageLayout.ResumeLayout(false);
+            this.grpboxHeader.ResumeLayout(false);
+            this.grpboxHeader.PerformLayout();
+            this.grpbxFooter.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel PageLayout;
+        private System.Windows.Forms.GroupBox grpboxHeader;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.GroupBox grpbxFooter;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
